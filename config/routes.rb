34779ff37645bda740927199d642a46cf56b978d
FredLavoie <flavoie3@gmail.com>
2019-07-12 11:24:57 +0000

    Implement user login and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :users
+  # resources :sessions
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -15,9 +18,17 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :categories, except: [:edit, :update, :show]
-    resources :products, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show]   
   end
+ 
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
